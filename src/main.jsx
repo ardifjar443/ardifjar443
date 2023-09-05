9be11340e313e9c61dfd303f837bcbb4e55022e7
ardi <ardifjar443@gmail.com>
2023-09-05 14:32:40 +0700

    percobaan_react2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import App2 from './App2.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>,
+)
+
+ReactDOM.createRoot(document.getElementById('ehe')).render(
+  <React.StrictMode>
+    <App2 />
   </React.StrictMode>,
 )
