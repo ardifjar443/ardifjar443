9ac399e6b66ede2805839ed887f54383c27b9587
ardi <ardifjar443@gmail.com>
2023-09-06 23:16:47 +0700

    new: ganti font

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import AppNavbar from "./components/Navbar";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import "./App.css";
+import "./index.css";
 
 function App() {
   const [activeTab, setActiveTab] = useState("home");
 
   return (
-    <div>
+    <div style={{ fontFamily: "Ubuntu Mono" }}>
       <AppNavbar activeTab={activeTab} setActiveTab={setActiveTab} />
       <Content activeTab={activeTab} setActiveTab={setActiveTab} />
       <p>{activeTab}</p>
