f5686d7509ee94a2df821f4ee1c64a48b02615c5
ardi <ardifjar443@gmail.com>
2023-09-07 13:12:57 +0700

    romban besar-besaran

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ardifjar443.github.io/",
+  base: "/ardifjar443/",
 });
